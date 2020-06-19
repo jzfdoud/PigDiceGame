e9347727b32c04036a9b2a9d2aab33ac05f6dace
Jada <jada.fagan@gmail.com>
2020-06-19 15:58:36 -0400

    Add project files.

---
+++ PigDice/PigDice.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PigDice
+{
+    class PigDice
+    {
+        Random rnd = new Random();
+        //public void Play()   for one game
+        public int Play()
+        {
+            var total = 0;
+            var die = Roll();
+            while(die !=1)
+            {
+                total += die;
+                die = Roll();
+            }
+            return total;
+            //Console.WriteLine($"Score is {total}.");
+        }
+        int Roll()
+        {
+            return rnd.Next(1, 7);
+        }
+
+    }
+}
---
+++ PigDice/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PigDice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pigdice = new PigDice();
+            var highscore = 0;
+            while (highscore < 713)
+            //for (var count = 1; count <= 1000000000; count++)
+            {
+                var score = pigdice.Play();
+                if (score > highscore)
+                {
+                    highscore = score;
+                }
+            }
+            Console.WriteLine($"High Score is {highscore}.");
+        }
+    }
+}
